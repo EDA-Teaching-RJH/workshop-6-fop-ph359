7ff94f5cd28c25b3e3d82c0be44501ba19497a10
Philip <ph359@kent.ac.uk>
2026-02-09 11:53:36 +0000

    Storage bay

--- part_1.py
+++ part_1.py
@@ -1,1 +1,17 @@
 
+def storage_bay():
+    #List varaible
+    sample_bay = ["Basalt","Silica","Iron","Dust"]
+
+    print("---initialising---")
+    print("------------------")
+    print("Collected Samples")
+    print("------------------")
+#Display variables
+    print(f"first Item: " + str(sample_bay[0]))
+    print(f"Last Item: " + str(sample_bay[-1]))
+    print(f"Total Items: " + str(len(sample_bay)))
+
+    print("------------------")
+
+storage_bay()
